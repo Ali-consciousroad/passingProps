d9b1fc97f6be30fe0005b52c9c01f5a1f1365396
ali-consciousroad <ali_dindar@live.be>
2024-07-19 15:55:19 +0200

    Add comments for better readability

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Heading from "./Heading";
 function App() {
     return (
         <div className="App">
+            {/* Pass data down from the parent component (App.js) to the child component (Heading.js) */}
             {/* Exemple of passing firstName and lastName as props to the Heading component */}
             <Heading firstName="Jean" lastName="Moule"/>
         </div>
